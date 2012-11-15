e36f33516ac8ad816c479d0490c153c33eb19387
Lance Velasco <lance@mlvgroup.com>
2012-11-14 18:50:44 -0800

    Buggs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,22 +2,22 @@ class SessionsController < ApplicationController
 
   def new
     @title = "Sign in"
-
   end
-
+  
   def create
-    user = User.find_by_email(params[:session][:email])
-    if user && User.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
-    else
-      flash.now[:error] = 'Invalid email/password combination'
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
       render 'new'
+    else
+      sign_in user
+      redirect_to user
     end
   end
-
+  
   def destroy
     sign_out
-    redirect_to root_url
+    redirect_to root_path
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,5 @@
 Factory.define :user do |user|
-  user.name                  "Lance Velasc"
+  user.name                  "Lance Velasco"
   user.email                 "lance@example.com"
   user.password              "foobar"
   user.password_confirmation "foobar"
