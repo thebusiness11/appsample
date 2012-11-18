f36ab5a99d2935474fe935bd23764d344b819b7e
Lance Velasco <lance@mlvgroup.com>
2012-11-18 15:28:37 -0800

    almost there

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -166,8 +166,80 @@ describe User do
         it "should have a feed" do
           @user.should respond_to(:feed)
         end
-        
       end
+    end
+    
+    describe "realtionships" do
+      
+      before(:each) do
+        @user = User.create!(@attr)
+        @followed = Factory(:user)
+      end
+      
+      
+      it "should have a realtionships method" do
+        @user.should respond_to(:relationships)
+      end
+      
+      it "should have a following method" do
+        @user.should respond_to(:following)
+      end
+    
+    it "shoild follow another user" do
+      @user.follow!(@followed)
+      @user.should be_following(@followed)
+    end
+    
+    it "should include the followed user in the following array" do
+      @user.follow!(@followed)
+      @user.following.should include(@followed)
+    end
+    
+    it "should have an unfollow! method" do
+      @user.should respond_to(:unfollow!)
+    end
+    
+    it "should unfollow a user" do
+      @user.follow!(@followed)
+      @user.unfollow!(@followed)
+      @user.should_not be_following(@followed)
+    end
+    
+    it "should have a reverse_relationships method" do
+        @user.should respond_to(:reverse_relationships)
+    end
+    
+    it "should have a followers method" do
+      @user.should respond_to(:followers)
+    end
+    
+    it "should include the follower in the followers array" do
+          @user.follow!(@followed)
+          @followed.followers.should include(@user)
+    end
+    
+    end
   end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
