674156cef593adb5cf158450d4d57b5ba4e86a5c
Lance Velasco <lance@mlvgroup.com>
2012-11-24 07:29:51 -0800

    Trash

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,10 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      respond_to  do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       @feed_items = []
       render 'pages/home'
